a020a0294443ae81ce50f2928d4b5f3ac647c1f4
iamsufiyan560 <140635114+iamsufiyan560@users.noreply.github.com>
2024-03-30 19:57:17 +0530

    create crud  api for  user  route

---
+++ backend/index.js
@@ -0,0 +1,24 @@
+//packages
+import path from "path";
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+
+//Utiles
+import connectDB from "./config/db.js";
+import userRoutes from "./routes/userRoutes.js";
+
+dotenv.config();
+const port = process.env.PORT || 5000;
+
+connectDB();
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.use("/api/users", userRoutes);
+
+app.listen(port, () => console.log(`server is running on port: ${port}`));
