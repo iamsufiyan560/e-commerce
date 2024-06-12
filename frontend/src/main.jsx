9750f3626b0fd489fc3c658ea5dcbb3c9f72cfa9
iamsufiyan560 <140635114+iamsufiyan560@users.noreply.github.com>
2024-06-12 22:47:34 +0530

    feat: Add CRUD API endpoints for products and corresponding UI

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -14,8 +14,10 @@ import Register from "./pages/Auth/Register";
 import Profile from "./pages/User/Profile";
 import AdminRoute from "./pages/Admin/AdminRoute.jsx";
 import UserList from "./pages/Admin/UserList.jsx";
-
+import ProductList from "./pages/Admin/ProductList.jsx";
 import CategoryList from "./pages/Admin/CategoryList";
+import AdminProductUpdate from "./pages/Admin/ProductUpdate.jsx";
+import AllProducts from "./pages/Admin/AllProducts.jsx";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -30,6 +32,9 @@ const router = createBrowserRouter(
       <Route path="/admin" element={<AdminRoute />}>
         <Route path="userlist" element={<UserList />} />
         <Route path="categorylist" element={<CategoryList />} />
+        <Route path="productlist" element={<ProductList />} />
+        <Route path="product/update/:_id" element={<AdminProductUpdate />} />
+        <Route path="allproductslist" element={<AllProducts />} />
       </Route>
     </Route>
   )
